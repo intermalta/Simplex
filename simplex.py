a97837a3002f5641e9faf2adc75d3155df9297f9
intermalta <intermalta@gmail.com>
2014-06-10 10:18:49 -0300

    working with chvatal problem

--- simplex.py
+++ simplex.py
@@ -4,8 +4,6 @@ from numpy import *
 import scipy as Sci
 import scipy.linalg
 
-def print_dictionary(Binv, N, b, cb, cn, xn):
-    return
 def validate_input(A, b, c):
     [m, n] = A.shape
     if (m != len(b)):
@@ -15,15 +13,15 @@ def validate_input(A, b, c):
     return 0
 
 def pivot(i, j, base, nbase):
-    aux = base[i]
-    # print aux
-    # print "variavel %d sai da base" %(base[i])
-    base[i] = nbase[j]
-    # print "variavel %d entra na base" %(nbase[j])
-    # print base
-    nbase[j] = aux
-    # print nbase
-    return base, nbase
+   
+    print 'base antes do pivoteamento -> ' + (base+1).__str__()
+    b = base.copy()
+    n = nbase.copy()
+    aux = b[i]
+    b[i] = n[j]
+    n[j] = aux
+    print 'base depois do pivoteamento -> ' + (b+1).__str__()
+    return b, n
 
 def revised_row(B, u, l):
     [m, n] = B.shape
@@ -48,101 +46,89 @@ def get_var_sainte(direcao, Binv, b):
     for i in range(len(u)):
         if u[i] <= 0:
             u[i] = nan
-    # print direcao
-    # print Binv
-    # print b
+
     temp = dot(Binv, b)
-    # print temp
     temp = temp / u
-    # print temp
+    
     return nanargmin(temp)
 
-def phase_one(Aext, b, bmin, cext, m, n):
+def phase_one(Aext, b, bmin, m, n):
         # adding w to A matrix at last column 
         w = ones((m, 1)) * (bmin)
         AwithW = concatenate((Aext, w), 1)
         # print AwithW
 
         # add -1 to w position in the new cost vector
-        cwithW = concatenate((zeros(m + n), ones(1) * bmin))
-        # print cwithW
+        cwithW = concatenate((zeros(m + n), ones(1) * (-1)))
 
         base1 = array([i for i in range(n, n + m)])
-        # print base1
+        
         nbase1 = array([i for i in range(0, n + 1)])
+        #colocando a variavel w no vetor de variaveis nao basicas
         nbase1[-1] = n + m
-        # print nbase1
 
         # adding w to the base
-        entrante = len(base1) - 1  
-        # print 'entrante %d' %entrante
+        entrante = len(nbase1) -1 
         sainte = argmin(b)
-        # print 'sainte %d' %sainte
         base1, nbase1 = pivot(sainte, entrante, base1, nbase1)
-        print base1, nbase1
 
-        Bwinv = revised_row(eye(m, n + 1), w, sainte)
+        Bwinv = revised_row(eye(m), w, sainte)
         tentativas = 200
         while tentativas > 0:
-            
-            Bw = AwithW[:, base1]
-            #print linalg.inv(Bw)
+            tentativas = tentativas - 1
             Nw = AwithW[:, nbase1]
-            #print Nw
 
             cbw = cwithW[:, base1]
             cnw = cwithW[:, nbase1]
 
-            
             yw = dot(cbw, Bwinv)
-            credw = cnw - dot(yw, b)
+            credw = cnw - dot(yw, Nw)
             xbw = dot(Bwinv, b)
             zw = dot(cbw, xbw)
             
             if max(cbw) < 0 or zw > 0:
                 print 'Problema inviavel'
+                #### XXX - excluir a coluna relativa a w
                 exit()
-            else:
-                if zw == 0:
-                    # we are at optimal z -> search for column w
-                    posw = -1
-                    for i in (range(len(nbase1))):
-                        if nbase1[i] == n + m:
-                            posw = i
-                    if posw == -1:
-                        print 'w not found in non bases vector'
-                        exit(3)
+            
+            if zw == 0:
+                # we are at optimal z -> search for column w
+                posw = -1
+                for i in (range(len(nbase1))):
+                    if nbase1[i] == n + m:
+                        posw = i
+                if posw == -1:
+                    print 'w not found in non bases vector'
+                    exit(3)
 
-                    Nw = delete(Nw, posw)
-                    # print Nw
-                    nbase1 = delete(nbase1, posw)
-                    print '====='
-                    print 'initial base'
-                    print base1
-                    print 'Fim fase 1 com %d iteracoes' % (200 - tentativas + 1)
-                    return base1, nbase1
+                Nw = delete(Nw, posw)
+                # print Nw
+                nbase1 = delete(nbase1, posw)
+                print '====='
+                print 'initial base'
+                print base1
+                print 'Fim fase 1 com %d iteracoes' % (200 - tentativas)
+                return base1, nbase1, Bwinv
                     
-            maximow = 0
-            for j in range(credw.shape[0]):
-                if credw[j] > maximow:
-                    maximow = credw[j]
+            posicaoColunaw = -1
+            for j in range(len(credw)):
+                if credw[j] > 0:
                     posicaoColunaw = j
                     break
-        
+            if posicaoColunaw == -1:
+                print 'nao achou uma coluna para entrar'
+                exit(-20)
             # direcao do deslocamento
             direcaow = dot(Bwinv, Nw[:, posicaoColunaw])
-            u = direcaow.copy()
-            # print "=========="
-            # print direcaow
             dwmax = direcaow.max()
         
             if dwmax < 0:
                 print "Problema Ilimitado"
             else:
-                posicaoLinhaw = get_var_sainte(u, Bwinv, b)
+                posicaoLinhaw = get_var_sainte(direcaow, Bwinv, b)
                 base1, nbase1 = pivot(posicaoLinhaw, posicaoColunaw, base1, nbase1)
             Bwinv = revised_row(Bwinv, direcaow, posicaoLinhaw)
-            tentativas = tentativas - 1
+            
 def simplex(A, b, c):
     
     if validate_input(A, b, c):
@@ -163,33 +149,27 @@ def simplex(A, b, c):
     # print bmin
     if bmin < 0:
         print "phase 1"
-        base, nbase = phase_one(Aext, b, bmin, cext, m, n)
+        first_fase = True
+        base, nbase, Binv = phase_one(Aext, b, bmin, m, n)
 
     else:
         base = array([i for i in range(n, n + m)])
         # print base
         nbase = array([i for i in range(0, n)])
         # print nbase
-    
-    B = Aext[:, base]
-    # print B
-    N = Aext[:, nbase]
-    # print N
-        
-    cb = cext[:, base]
-    # print cb
-    cn = cext[:, nbase]
-    # print cn
-        
-    ###### MUDAR AS INVERSOES DE MATRIZES #######
-    Binv = B.copy()
+    if first_fase == False:
+        B = Aext[:, base]
+        Binv = B.copy()
     # print Binv
-
 
     # simplex fase 2
     print 'inicio fase 2'
     tentativas = 200
     while tentativas > 0:
+        tentativas = tentativas -1
+        N = Aext[:, nbase]
+        cb = cext[:, base]
+        cn = cext[:, nbase]
         y = dot(cb, Binv)
         # print y
         cred = cn - dot(y, N)
@@ -201,41 +181,33 @@ def simplex(A, b, c):
             xb = dot(Binv, b)
             z = dot(cb, xb)
             print base, nbase
-            print 'Fim fase 2 - %d iteracoes' % (200 - tentativas + 1)
+            print 'Fim fase 2 - %d iteracoes' % (200 - tentativas)
             return xb, z, y
-        else:
 
-            # anti cycle - lexicographic order
-            maximo = 0
-            for j in range(cred.shape[0]):
-                if cred[j] > maximo:
-                    maximo = cred[j]
-                    posicaoColuna = j
-                    break
+        # anti cycle - Bland's rule
+        posicaoColuna = -1
+        for j in range(len(cred)):
+            if cred[j] > 0:
+                #maximo = cred[j]
+                posicaoColuna = j
+                break
+        if posicaoColuna == -1:
+            print 'Erro ao achar a variavel que entra na base'
+            exit(-3)
+        # direcao do deslocamento
+        direcao = dot(Binv, N[:, posicaoColuna])
+        dmax = direcao.max()
+    
+        if dmax < 0:
+            print "Problema Ilimitado"
+            exit()
+        else:
+            posicaoLinha = get_var_sainte(direcao, Binv, b)
+            base, nbase = pivot(posicaoLinha, posicaoColuna, base, nbase)
         
-            # direcao do deslocamento
-            direcao = dot(Binv, N[:, posicaoColuna])
+        Binv = revised_row(Binv, direcao, posicaoLinha)
             
-            # print "=========="
-            # print direcao
-            dmax = direcao.max()
-        
-            if dmax < 0:
-                print "Problema Ilimitado"
-                exit()
-            else:
-                posicaoLinha = get_var_sainte(direcao, Binv, b)
-                base, nbase = pivot(posicaoLinha, posicaoColuna, base, nbase)
-            
-            Binv = revised_row(Binv, direcao, posicaoLinha)
-            #print Binv
-            
-            cb = cext[:, base]
-            # print cb
-            cn = cext[:, nbase]
-            # print cn
-            
-            tentativas = tentativas - 1
+          
 if __name__ == "__main__":
     # c = array([4, 3])
     # A = array([[2, 1], [1,2]])
@@ -248,6 +220,13 @@ if __name__ == "__main__":
     c = array([4, 3])
     A = array([[2, 1], [1, 2], [-1, -1]])  
     b = array([4, 4, -1])
+    
+    
+    #########Chvatal pg 39
+    #c = array([1,-1,1])
+    #A = array([[2,-1,2],[2,-3,1], [-1,1,-2]])
+    #b = array([4,-5,-1])
+    
     xb, z, y = simplex(A, b, c)
   
     # #teste matriz revisada
